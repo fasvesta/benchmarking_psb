07c703c8787be1e1639bcad623897e668633f62e
fasvesta <as.fotini@gmail.com>
2022-12-20 16:22:03 +0100

    typo & condor files

---
+++ condor_executable.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# export EOS_MGM_URL=root://eosuser.cern.ch
+
+source /usr/local/xsuite/miniforge3/bin/activate xsuite
+pwd
+
+cp -r /afs/cern.ch/work/f/fasvesta/benchmarking_psb/PSB .
+cp /afs/cern.ch/work/f/fasvesta/benchmarking_psb/*.py .
+mkdir output
+
+python examplePSBPIC.py
+mkdir /eos/user/f/fasvesta/benchmarking_psb/output_524
+xrdcp output/* root://eosuser.cern.ch//eos/user/f/fasvesta/benchmarking_psb/output_524/.
