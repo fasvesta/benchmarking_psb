4a2b892dff2ec4351f468d7880900aaaa09340dd
fasvesta <as.fotini@gmail.com>
2023-02-15 13:03:43 +0100

    factor_beta for delta

--- examplePSBPIC.py
+++ examplePSBPIC.py
@@ -14,8 +14,8 @@ import time
 # Choose a context #
 ####################
 
-#context = xo.ContextCpu()
-context = xo.ContextCupy()
+context = xo.ContextCpu()
+#context = xo.ContextCupy()
 #context = xo.ContextPyopencl('0.0')
 
 print(context)
@@ -33,11 +33,11 @@ line.particle_ref=xp.Particles(mass0=xp.PROTON_MASS_EV,
 nemitt_x=3e-6
 nemitt_y=2.3e-6
 bunch_intensity=55e10
-sigma_z=16.9
-n_part = int(20e3)
+sigma_z=16.9#15.85*0.67#
+n_part = int(200e3)
 
 # from space charge example
-num_turns= int(150e3)
+num_turns= int(1)
 
 num_spacecharge_interactions = 160 # is this interactions per turn?
 tol_spacecharge_position = 1e-2 # is this the minimum/maximum space between sc elements?
@@ -97,17 +97,17 @@ tracker_sc_off = tracker.filter_elements(exclude_types_starting_with='SpaceCh')
 # Generate particles #
 ######################
 
-#particles = xp.generate_matched_gaussian_bunch(_context=context, num_particles=n_part,
-#                            total_intensity_particles=bunch_intensity,
-#                            nemitt_x=nemitt_x, nemitt_y=nemitt_y, sigma_z=sigma_z,
-#                            particle_ref=line.particle_ref,
-#                            tracker=tracker_sc_off)
-
-particles = parabolic_longitudinal_distribution(_context=context, num_particles=n_part,
+particles = xp.generate_matched_gaussian_bunch(_context=context, num_particles=n_part,
                             total_intensity_particles=bunch_intensity,
                             nemitt_x=nemitt_x, nemitt_y=nemitt_y, sigma_z=sigma_z,
                             particle_ref=line.particle_ref,
                             tracker=tracker_sc_off)
+
+#particles = parabolic_longitudinal_distribution(_context=context, num_particles=n_part,
+#                            total_intensity_particles=bunch_intensity,
+#                            nemitt_x=nemitt_x, nemitt_y=nemitt_y, sigma_z=sigma_z,
+#                            particle_ref=line.particle_ref,
+#                            tracker=tracker_sc_off)
 
 
 #monitor = xt.ParticlesMonitor(_context=context,
@@ -116,22 +116,24 @@ particles = parabolic_longitudinal_distribution(_context=context, num_particles=
 #                              repetition_period=500,
 #                              num_particles=n_part)
 
-r=StatisticalEmittance(context='GPU')
+r=StatisticalEmittance(context='CPU')
 bunch_moments=r.measure_bunch_moments(particles)
 print(bunch_moments['nemitt_x'])
 print(bunch_moments['nemitt_y'])
 output=[]
 start = time.time()
+np.save('distribution_in_hi_g', r.coordinate_matrix)
+'''
 #tracker.track(particles, num_turns=num_turns, turn_by_turn_monitor=monitor)
 for i in range(num_turns):
     tracker.track(particles)
     bunch_moments=r.measure_bunch_moments(particles)
     output.append([len(r.coordinate_matrix[0]),bunch_moments['nemitt_x'].tolist(),bunch_moments['nemitt_y'].tolist(),bunch_moments['emitt_z'].tolist()])
-    if i in range(-1, num_turns, 1000):
-        if r.context=='GPU':
-            np.save('output/distribution_'+str(int(i)), r.coordinate_matrix.get())
-        else:
-            np.save('output/distribution_'+str(int(i)), r.coordinate_matrix)
+    #if i in range(-1, num_turns, 1000):
+    #    if r.context=='GPU':
+    #        np.save('output/distribution_'+str(int(i)), r.coordinate_matrix.get())
+    #    else:
+    np.save('distribution_'+str(int(i)), r.coordinate_matrix)
 end = time.time()
 bunch_moments=r.measure_bunch_moments(particles)
 print('epsn_x = ',bunch_moments['nemitt_x'])
@@ -139,4 +141,5 @@ print('epsn_y = ',bunch_moments['nemitt_y'])
 print('eps_z = ',bunch_moments['emitt_z'])
 print('time = ', end - start)
 ouput=np.array(output)
-np.save('output/emittances', output)
+#np.save('output/emittances', output)
+'''
--- parabolic_longitudinal_distribution.py
+++ parabolic_longitudinal_distribution.py
@@ -7,10 +7,10 @@ def parabolic_longitudinal_distribution(_context=None, num_particles=None,
                 nemitt_x=None, nemitt_y=None, sigma_z=None,
                 particle_ref=None, tracker=None, total_intensity_particles=None):
 
-    zeta, delta = generate_longitudinal_coordinates(distribution='parabolic', 
+    zeta, delta = generate_longitudinal_coordinates(distribution='gaussian', 
                 num_particles=num_particles, 
                 engine='single-rf-harmonic', sigma_z=sigma_z,
-                particle_ref=particle_ref, tracker=tracker)
+                                                    particle_ref=particle_ref, tracker=tracker)
 
     x_norm = np.random.normal(size=num_particles)
     px_norm = np.random.normal(size=num_particles)
@@ -21,7 +21,7 @@ def parabolic_longitudinal_distribution(_context=None, num_particles=None,
         total_intensity_particles = num_particles
 
     particles = build_particles(_context=_context,  particle_ref=particle_ref,
-                tracker=tracker, zeta=zeta, delta=delta, 
+                tracker=tracker, zeta=zeta, delta=delta*particle_ref._xobject.beta0[0], 
                 x_norm=x_norm, px_norm=px_norm,
                 y_norm=y_norm, py_norm=py_norm,
                 scale_with_transverse_norm_emitt=(nemitt_x, nemitt_y),
